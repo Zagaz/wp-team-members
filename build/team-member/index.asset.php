80ad0c02a93db278db655548579046f79849aec9
Zagaz <andre_ranulfo@hotmail.com>
2024-05-03 15:40:41 -0300

    🧪 Cusrtom alt text side bar tested

--- build/team-member/index.asset.php
+++ build/team-member/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd0596b8d622125dcbfab');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0453703528ecab68ba73');
